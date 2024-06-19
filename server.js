628750006fbdbfa4064beb7345890c4b0a4e068f
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2024-06-19 14:13:25 -0300

    Método post criado

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const app = express();
 const PORT = 3000;
 app.use(express.json())
 
+const users = []
+
 app.get("/users", (req, res) => {
 
-    res.send("Olá")
+    res.json(users)
 })
 
 app.post("/users", (req, res) => {
+    users.push(req.body)
 
-    res.json()
+    res.status(201).json({message: "Usuário criado com sucesso!"})
 })
 
 app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}`))
